ea390b3206c42c575c777cc4fe02b962e2c32eb9
rickknowlton <richard.d.knowlton@gmail.com>
2018-11-24 23:57:00 -0500

    v 1.0.4

--- server.js
+++ server.js
@@ -33,15 +33,20 @@ router.get('/*', (req,res) =>{
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(router);
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
-const db = process.env.MONGODB_URI || 'mongodb://localhost/newsy';
-mongoose.connect(db,
-    (err)=>{
-    if (err) throw err;
-    console.log('connection is successful')
-})
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-app.listen(PORT, ()=>{
-    console.log('http://localhost:'+PORT);
-})
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
